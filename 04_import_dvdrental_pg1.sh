6dbb0b2427f0b289f758027d9bfe27f19a0cc08b
Sergio Romera <sergio.romera@enterprisedb.com>
2023-09-01 11:48:56 +0200

    Version 1.1

--- 04_import_dvdrental_pg1.sh
+++ 04_import_dvdrental_pg1.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
 kubectl port-forward pg1-1 5432:5432 &
+pid=$!
+echo "Port forward process id: $pid"
 sleep 3
 export PGPASSWORD=$(kubectl get secret --namespace default pg1-app -o jsonpath="{.data.password}" | base64 --decode)
 pg_restore -h localhost -U app -d app -p 5432 -O dvdrental.tar
+kill $pid
 
 # Export sequences id
 echo "Export sequences id's"
-rm sequences.txt
+rm sequences.sql
 kubectl exec -it pg1-1 -- psql app -t -c "select 'select setval(''' || sequencename ||''',' || last_value||');' from pg_sequences;" > sequences.sql
 kubectl exec -it pg1-1 -- psql app -c "\d"
 
--- 99_remove_all.sh
+++ 99_remove_all.sh
@@ -3,4 +3,4 @@
 kubectl delete cluster pg1
 kubectl delete cluster pg2
 kubectl delete secrets logicalrepuser
-
+rm sequences.sql
